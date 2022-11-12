0624b53710fe842d1c4e8ce423aeb8cee49252e8
stephennemeth <softwareprojectfinder@gmail.com>
2022-11-11 21:32:55 -0500

    Updates

---
+++ precache-manifest.51ffd5bc066d268af00f44b17e226e99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6eb200737d5011bf7e2",
+    "url": "/softwareprojectfinder/static/css/main.2da38f27.chunk.css"
+  },
+  {
+    "revision": "f6eb200737d5011bf7e2",
+    "url": "/softwareprojectfinder/static/js/main.f6eb2007.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/softwareprojectfinder/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "c0f5af3608b0f06e5698",
+    "url": "/softwareprojectfinder/static/css/2.646f0b5b.chunk.css"
+  },
+  {
+    "revision": "c0f5af3608b0f06e5698",
+    "url": "/softwareprojectfinder/static/js/2.c0f5af36.chunk.js"
+  },
+  {
+    "revision": "20f24e389dbbadadf9fe",
+    "url": "/softwareprojectfinder/static/js/runtime~main.20f24e38.js"
+  },
+  {
+    "revision": "86020da81bd3437ed84fc76d2e731b06",
+    "url": "/softwareprojectfinder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3473e2f86e0bc00e2302d0a1c379e7ab.js"
+  "/softwareprojectfinder/precache-manifest.51ffd5bc066d268af00f44b17e226e99.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/softwareprojectfinder/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
